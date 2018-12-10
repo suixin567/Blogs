256da58b303eb3fd431b4c611c0d3875bfaa2481
ljj <591821945@qq.com>
2018-12-10 17:50:04 +0800

    利用模板函数显示兴趣

--- main.go
+++ main.go
@@ -4,16 +4,25 @@ import (
 	"DDN_XS/models/class"
 	_ "DDN_XS/routers"
 	"encoding/gob"
+	"strings"
 
 	"github.com/astaxie/beego"
 )
 
 func init() {
 	gob.Register(class.User{})
+	beego.AddFuncMap("split", SplitHobby)
+
 }
 func main() {
 	//	beego.SetStaticPath("img", "./views/img")
 	//	beego.SetStaticPath("css", "./views/css")
 	//	beego.SetStaticPath("js", "./views/js")
 	beego.Run()
+}
+
+/*	Template Function	*/
+
+func SplitHobby(s string, sep string) []string {
+	return strings.Split(s, sep)
 }
